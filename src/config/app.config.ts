22c32c9e913a627275aca6440778ed7f9b1afad4
georgolden <georgoldenb@gmail.com>
2025-01-09 17:01:20 +0100

    update default dev values

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -3,5 +3,5 @@ import 'dotenv/config';
 const { env } = process;
 
 export const HOST = env.HOST || 'localhost';
-export const PORT = Number.parseInt(env.PORT, 10) || 3000;
-export const CORS_ORIGIN = env.CORS_ORIGIN || 'http://localhost:3002';
+export const PORT = Number.parseInt(env.PORT, 10) || 3002;
+export const CORS_ORIGIN = env.CORS_ORIGIN || 'http://localhost:5173';
